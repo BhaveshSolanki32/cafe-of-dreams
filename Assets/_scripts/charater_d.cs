498f2270a2f11d8f2c8349d439137e97816883f7
BhaveshSolanki32 <solankibhavesh0149@gmail.com>
2023-01-10 02:23:12 +0530

    last day

--- Assets/_scripts/charater_d.cs
+++ Assets/_scripts/charater_d.cs
@@ -8,17 +8,28 @@ public class charater_d : MonoBehaviour
     public bool seeded;
     public List<int> seed_type;
     public Sprite dp;
+    int seed_score = 0;
 
     [Header("METER")]
     [Range(0,1)] public float drink;
     [Range(0, 1)] public float seed;
     [Range(0, 1)] public float fear;
     [Range(0, 1)] public float sanity;
-
+    [Header("TAGS")]
+    List<string> _tags;
     public string age;
     public string occup;
-    public void seed_enabler()
+    public void seedScore(seeds seed)
     {
-
+        foreach(string x in seed.pos_tags)
+        {
+            if (_tags.Contains(x))
+                seed_score += 1;
+        }
+        foreach (string x in seed.neg_tags)
+        {
+            if (_tags.Contains(x))
+                seed_score -= 1;
+        }
     }
 }
